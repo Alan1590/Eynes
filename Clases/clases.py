5522f46dfcd478d9eef1b79c16d89b9c92f3a94b
Alan1590 <awg.1590@gmail.com>
2020-10-30 13:41:14 -0300

    First commit

---
+++ Clases/clases.py
@@ -0,0 +1,15 @@
+class Circulo:
+
+    def __init__(self, value):
+        pass
+
+    def get_area(self,value):
+        pass
+
+    def get_perimetro(self,value):
+        pass
+
+import doctest
+doctest.testmod()
+
+Circulo()
