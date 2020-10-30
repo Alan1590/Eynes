7e2873f1760f19f9778eb804c5f395696eefb45b
Alan1590 <awg.1590@gmail.com>
2020-10-30 13:24:41 -0300

    first commit

---
+++ Simple/simple.py
@@ -0,0 +1,17 @@
+from random import randrange
+
+
+class Simple:
+    def iniciar(self):
+        pass
+
+    def get_list(self, n_element):
+        pass
+
+    def order_list(self, list_element):
+        pass
+
+#import doctest
+#doctest.testmod()
+
+Simple().iniciar()
