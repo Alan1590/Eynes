1b08a731c694c1746fde1cefea3a88f2c7c3b501
Alan1590 <awg.1590@gmail.com>
2020-10-30 13:29:24 -0300

    first commit

---
+++ Matriz/matriz.py
@@ -0,0 +1,13 @@
+class Matriz():
+
+    def iniciar(self):
+        pass
+
+    def matriz(self, m):
+        pass
+
+    def get_final_result(self, result):
+        pass
+
+    def element_to_search(self, matriz, element, pos_x, pos_y):
+        pass
